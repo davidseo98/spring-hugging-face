ff0cb5250727075295460b79e29e153c25173214
davidseo98 <hyundukseo98@gmail.com>
2024-07-16 20:45:47 +0900

    initial commit

---
+++ src/main/java/com/demo/hugging_face/HuggingFaceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hugging_face;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuggingFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuggingFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/hugging_face/HuggingFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.hugging_face;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuggingFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
